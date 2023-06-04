5d8d210ab58391734f2f3a340e7bb1952a3fb040
mtohernandez <m4teoh3rnandez@outlook.com>
2023-06-04 09:16:05 -0500

    First Commit

---
+++ src/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// Home screen containing the header, banner, and rows of movies
+
+const HomeScreen = () => {
+  return (
+    <div className='homeScreen'>
+      {/* Header */}
+
+      {/* Banner */}
+
+      {/* Row */}
+    </div>
+  )
+}
+
+export default HomeScreen
