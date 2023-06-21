600d89a1e7a65b8abee724adcdb5075022a94552
mtohernandez <m4teoh3rnandez@outlook.com>
2023-06-21 17:05:24 -0500

    Add initial version

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom"
+
+function App() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/router.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/Home",
+        element: <Home />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+export default router;
