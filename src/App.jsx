96f43e66fd112fb6227dbe60d59caf792b5cf1a8
Mateo Hernandez <m4teoh3rnandez@outlook.com>
2026-03-16 14:11:27 -0500

    simplify app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,8 @@
-import { Outlet, useNavigate } from "react-router-dom";
-import { Nav, Footer } from "./components";
-import { useEffect } from "react";
-import { login, logout, selectUser } from "./features/userSlice";
-import { auth } from "./firebase";
-import { onAuthStateChanged } from "firebase/auth";
-import { useDispatch, useSelector } from "react-redux";
+import { RouterProvider } from 'react-router-dom';
+import router from '@/router';
 
 function App() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const user = useSelector(selectUser);
-
-  // The user needs to be logged in to access the app
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        dispatch(
-          login({
-            uid: user.uid,
-            email: user.email,
-          })
-        );
-        navigate("/home");
-      } else {
-        dispatch(logout());
-        navigate("/welcome/login");
-      }
-    });
-
-    // Cleanup function to unsubscribe from the listener
-    return unsubscribe;
-  }, [dispatch, navigate]);
-
-  return (
-    <div style={{minHeight: "100vh"}}>
-      <Nav user={user} />
-      <Outlet />
-      <Footer />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
