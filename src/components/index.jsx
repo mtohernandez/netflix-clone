1e2440737bf7f937c0bea9cfe22e1305db795587
Mateo Hernandez <m4teoh3rnandez@outlook.com>
2026-03-16 14:09:10 -0500

    remove old component barrel export

--- src/components/index.jsx
+++
@@ -1,6 +0,0 @@
-import Nav from "./common/Nav";
-import Footer from "./common/Footer";
-import LoginForm from "./custom/LoginForm";
-import Banner from "./custom/Banner";
-
-export { Nav, Footer, LoginForm, Banner };
