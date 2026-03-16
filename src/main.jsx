38ed0aeeeed217e48c74464f770f36fdc07a7492
Mateo Hernandez <m4teoh3rnandez@outlook.com>
2026-03-16 14:11:46 -0500

    rewrite entry point with query client provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,48 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
-import { RouterProvider } from "react-router-dom";
-import router from "./Router";
-import { fetchMovies, fetchMoviesAction, setErrorMovies } from "./features/moviesSlide";
-import {
-  fetchMovieImage,
-  fetchMovieVideoId,
-  fetchMovieVideo,
-  setMovie,
-} from "./features/movieSlide";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { store } from '@/store/store';
+import App from '@/App';
+import './main.css';
 
-import "./main.css";
-
-const dispatchFeaturedMovie = async (results, index) => {
-  try {
-    const movie = results.payload[index];
-    store.dispatch(setMovie(movie));
-    await store.dispatch(fetchMovieImage(movie.id));
-    const response = await store.dispatch(fetchMovieVideoId(movie.id));
-    await store.dispatch(fetchMovieVideo(response.payload));
-  } catch (error) {
-    throw new Error(error);
-  }
-};
-
-store.dispatch(fetchMovies()).then((results) => {
-  dispatchFeaturedMovie(
-    results,
-    Math.floor(Math.random() * results.payload.length - 1)
-  ).catch((error) => {
-    store.dispatch(setErrorMovies(error.message));
-  });
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
 });
 
-store.dispatch(fetchMoviesAction()).catch((error) => {
-  store.dispatch(setErrorMovies(error.message));
-});
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </QueryClientProvider>
+  </StrictMode>
 );
